18dfd9a341bd3fc8b2217d95e590ba58d65af9c1
Haddawi <muhamadhaddawi@gmail.com>
2021-03-23 23:27:04 +0700

    feature user-api: error handling

--- handler/user.go
+++ handler/user.go
@@ -27,13 +27,17 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	err := c.ShouldBindJSON(&input)
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
+		response := helper.APIResponse("Failed to create account!", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
 	}
 
 	newUser, err := h.userService.RegisterUser(input)
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
+		response := helper.APIResponse("Failed to create account!", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
 	}
 
 	formatter := user.FormatUser(newUser, "token")
